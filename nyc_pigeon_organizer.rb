4230696a8b56a55337762776b185279532acef37
Taryn Johnson <taryn.johnson919@gmail.com>
2019-12-08 23:48:40 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,21 @@
+# The data above is organized by attributes - `:color`, `:gender`, and `:lives`,
+# each a hash. Within these hashes are arrays of pigeon names.
 
+# We want this data, but in a different structure. Rather than sorting by
+# attributes, your task is to create a hash where _pigeon names_ are the keys,
+# with each name pointing to a hash of their attributes.
+
+# Iterate over the hash above, collecting each pigeon by name and insert it as the
+# **key** of a new hash. Each of these hashes should have `:color`, `:gender`, and
+# `:lives` keys assigned to arrays of info about that particular pigeon. Your
+# output should look something like the hash below:
+
+# ```ruby
+# pigeon_list = {
+#   "Theo" => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Subway"]
 
 
 def nyc_pigeon_organizer(data)
